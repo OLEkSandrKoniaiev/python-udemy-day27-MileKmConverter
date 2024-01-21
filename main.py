508e9abb7a0126bdb88fcd1a1fca67bd6d0cc1c0
OLEkSandrKoniaiev <oleksandrkoniaiev.r@gmail.com>
2024-01-21 03:02:50 +0200

    final result;

--- main.py
+++ main.py
@@ -2,10 +2,31 @@ from tkinter import *
 
 
 def button_clicked():
-    pass
+    miles = float(miles_input.get())
+    km = round(miles * 1.609)
+    km_output.config(text=f"{km}")
 
 
 window = Tk()
-window.title("My First GUI Program")
+window.title("Miles to Kilometer Converter")
+window.config(padx=20, pady=20)
+
+miles_input = Entry(width=10)
+miles_input.grid(column=1, row=0)
+
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0)
+
+just_label = Label(text="is equal to")
+just_label.grid(column=0, row=1)
+
+km_output = Label(text="0")
+km_output.grid(column=1, row=1)
+
+km_label = Label(text="Km")
+km_label.grid(column=2, row=1)
+
+conv_button = Button(text="Calculate", command=button_clicked)
+conv_button.grid(column=1, row=2)
 
 window.mainloop()
